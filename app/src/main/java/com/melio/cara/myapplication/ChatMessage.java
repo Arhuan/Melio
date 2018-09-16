0dad9b0aa00889d45d313e407355812de0e59de5
Aaron Huang <aaronhuangwork@gmail.com>
2018-09-15 18:02:43 -0700

    created ChatMessage class

---
+++ app/src/main/java/com/melio/cara/myapplication/ChatMessage.java
@@ -0,0 +1,22 @@
+package com.melio.cara.myapplication;
+
+/**
+ * Created by aahuangg on 2018-09-15.
+ */
+
+public class ChatMessage {
+    private String messageBody;
+    private String sender;
+
+    public ChatMessage() {
+
+    }
+
+    public String getMessageBody() {
+        return this.messageBody;
+    }
+
+    public String getSender() {
+        return this.sender;
+    }
+}
