929d4ba7dd85cb624d7b4435903135039768f676
Evoiis <athonly@gmail.com>
2018-09-16 08:59:05 -0700

    Forum Adapter

--- app/src/main/java/com/melio/cara/myapplication/ForumList.java
+++ app/src/main/java/com/melio/cara/myapplication/ForumList.java
@@ -10,11 +10,11 @@ import android.widget.TextView;
 public class ForumList extends ArrayAdapter<String> {
 
     private final Activity context;
-    private final String postheader;
-    private final String postbody;
+    private final String[] postheader;
+    private final String[] postbody;
 
     public ForumList(Activity context,
-                     String postheader, String postbody) {
+                     String[] postheader, String[] postbody) {
         super(context, R.layout.item_posting);
         this.context = context;
         this.postheader = postheader;
@@ -28,8 +28,8 @@ public class ForumList extends ArrayAdapter<String> {
         View rowView = inflater.inflate(R.layout.item_posting, null, true);
         TextView txtHeader = (TextView) rowView.findViewById(R.id.header);
         TextView txtBody = (TextView) rowView.findViewById(R.id.body);
-        txtHeader.setText(postheader);
-        txtBody.setText(postbody);
+        txtHeader.setText(postheader[position]);
+        txtBody.setText(postbody[position]);
         return rowView;
     }
 }
