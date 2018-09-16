f52118dd32651aac6a83f39b1d22175ad90971d1
Aaron Huang <aaronhuangwork@gmail.com>
2018-09-15 20:55:57 -0700

    added layouts and drawables for sent and received messages

--- app/src/main/java/com/melio/cara/myapplication/ChatMessage.java
+++ app/src/main/java/com/melio/cara/myapplication/ChatMessage.java
@@ -1,22 +1,43 @@
 package com.melio.cara.myapplication;
 
+import java.util.Date;
+
 /**
  * Created by aahuangg on 2018-09-15.
  */
 
 public class ChatMessage {
-    private String messageBody;
+    private String body;
     private String sender;
+    private long timeSent;
 
-    public ChatMessage() {
-
+    public ChatMessage(String text, String sender) {
+        this.body = text;
+        this.sender = sender;
+        this.timeSent = new Date().getTime();
     }
 
-    public String getMessageBody() {
-        return this.messageBody;
+    public void setBody(String text) {
+        this.body = text;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setTimeSent(long time) {
+        this.timeSent = time;
+    }
+
+    public String getBody() {
+        return this.body;
     }
 
     public String getSender() {
         return this.sender;
+    }
+
+    public long getTimeSent() {
+        return this.timeSent;
     }
 }
