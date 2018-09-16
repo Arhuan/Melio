978ab3c7fca0bfd45c52ccb91f0ea2b005d3f7b6
Miraculin <colton.cheung@hotmail.ca>
2018-09-15 22:25:44 -0700

    removed implements

--- app/src/main/java/com/melio/cara/myapplication/LoginActivity.java
+++ app/src/main/java/com/melio/cara/myapplication/LoginActivity.java
@@ -7,7 +7,7 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;    
+import android.support.v7.app.AppCompatActivity;
 import android.os.AsyncTask;
 
 import android.os.Build;
