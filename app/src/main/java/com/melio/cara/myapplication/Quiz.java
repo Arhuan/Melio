44c7a2a57132f329dbced957ad6cfd9f0717a84d
Cinnanut <rain.bowl@live.ca>
2018-09-16 04:31:56 -0700

    finally got quiz results to update on database

--- app/src/main/java/com/melio/cara/myapplication/Quiz.java
+++ app/src/main/java/com/melio/cara/myapplication/Quiz.java
@@ -1,6 +1,8 @@
 package com.melio.cara.myapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,9 +12,17 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Quiz extends AppCompatActivity {
     private String[] question = new String[]
@@ -30,21 +40,22 @@ public class Quiz extends AppCompatActivity {
                     "I was scared that I would lose control, go crazy, or die."
             };
 
-    private int[] answers = new int[12];
-    private int x = 0;
+    public int x = 0;
     private TextView questionText;
     private RadioGroup radioGroup;
     private int selected = -1;
-    private int normal = 2131230848;
-
+    private int normal = 2131230858;
+    private updateAnswers uAnswers;
+    private DatabaseReference databaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        databaseRef = FirebaseDatabase.getInstance().getReference("users");
         setContentView(R.layout.activity_quiz);
 
         questionText = findViewById(R.id.questionText);
-        update();
+        update(selected);
 
         radioGroup = findViewById(R.id.radioGroup);
         Button nextButton = findViewById(R.id.nextButton);
@@ -56,25 +67,68 @@ public class Quiz extends AppCompatActivity {
                 {
                     selected = radioGroup.getCheckedRadioButtonId() - normal;
                     Log.d("Quiz.java TESTING:   ", "checked button: " + selected);
-                    x++;
-                    update();
+                    update(selected);
                 }
             }
         });
     }
 
-    public void update(){
+    public void update(int picked){
         if(x < question.length){
             questionText.setText(question[x]);
-            if (selected != -1){
-                answers[x] = selected;
+            if (picked != -1){
+                uAnswers = new Quiz.updateAnswers(picked, x);
+                uAnswers.execute((Void) null);
                 radioGroup.clearCheck();
+                x++;
             }
-            Log.d("Quiz.java TESTING:   ", "answers: " + answers);
         }
         else{
-            Log.d("Quiz.java TESTING:   ", "answers: " + answers);
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+        }
+    }
+
+    public class updateAnswers extends AsyncTask<Void, Void, Boolean> {
+        //Log.v("msg"","Hello")
+        private int answers;
+        private String index;
+
+        updateAnswers(int inpAnswers, int fax) {
+            answers = inpAnswers;
+            index = String.valueOf(fax);
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            Query query = databaseRef.child(localUsernameSingleton.getKnownInstance().LocalUsername);
+            query.addListenerForSingleValueEvent(new ValueEventListener(){
+
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()){
+                        databaseRef.child(localUsernameSingleton.getKnownInstance().LocalUsername).child("answers").child(index).setValue(answers);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+                }
+            });
+
+            return true;
+        }
+
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+        }
+
+        @Override
+        protected void onCancelled() {
         }
     }
 }
+
