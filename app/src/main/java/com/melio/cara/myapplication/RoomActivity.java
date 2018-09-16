c53d0e78884c38f4057f702d7c457935029abad1
Aaron Huang <aaronhuangwork@gmail.com>
2018-09-16 03:10:32 -0700

    commiting minor changes

--- app/src/main/java/com/melio/cara/myapplication/RoomActivity.java
+++ app/src/main/java/com/melio/cara/myapplication/RoomActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class RoomsActivity extends AppCompatActivity {
+public class RoomActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +15,6 @@ public class RoomsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rooms);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
